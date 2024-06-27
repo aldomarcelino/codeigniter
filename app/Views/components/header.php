074454789368b83b1909e4114bd8752fdad6862d
aldomarcelino <aldo115marcelino@gmail.com>
2024-06-27 10:06:55 +0700

    update redirect

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -2,7 +2,7 @@
 <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
+        <a href="/" class="logo d-flex align-items-center">
             <img src="<?php echo base_url() ?>NiceAdmin/assets/img/logo.png" alt="">
             <span class="d-none d-lg-block">Toko</span>
         </a>
